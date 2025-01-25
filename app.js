73b4eac7490704c2fb9a41b4117d7ed0df5fd063
João Marcos Martins <joaomarcosmartins217@gmail.com>
2025-01-25 11:14:48 -0300

    Criado array de nomes.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-function adicionarAmigo(){
+let nomeAmigo = [];
 
+function adicionarAmigo(){
+    
 }
 
 function sortearAmigo(){
