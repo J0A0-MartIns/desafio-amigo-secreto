ac6a2eb8f735dcf61454eb22695ccc06f50fab18
João Marcos Martins <joaomarcosmartins217@gmail.com>
2025-01-26 15:57:48 -0300

    Adicionar funções de adição e sorteio de amigos

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let nomeAmigo = [];
 
-function adicionarAmigo(){
-    
+//Adiciona o nome que foi inserido no input de id "amigo".
+//Não permite a inserção de nomes iguais.
+function adicionarAmigo() {
+  let nomeInformado = document.getElementById("amigo").value;
+  atualizarListaDeAmigos(nomeInformado);
 }
 
-function sortearAmigo(){
-    
+function atualizarListaDeAmigos(nomeInformado) {
+  if (nomeAmigo.includes(nomeInformado)) {
+    alert("Nome já inserido.");
+  } else {
+    nomeAmigo.push(nomeInformado);
+    alert(nomeInformado + " adicionado com sucesso!");
+    console.log(nomeAmigo);
+  }
+}
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+function sortearAmigo() {
+  let sorteio = nomeAmigo[parseInt(Math.random() * nomeAmigo.length + 1)];
+  exibirTextoNaTela("h2", "Você saiu com:");
+  exibirTextoNaTela("p", sorteio);
 }
